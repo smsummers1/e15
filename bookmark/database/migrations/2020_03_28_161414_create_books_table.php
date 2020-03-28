f1cc544188d4edd8163716fcfaf49c20f764320d
shawna summers <shawnamariesummers@gmail.com>
2020-03-28 15:03:50 -0400

    upgrading to Laravel 7

---
+++ bookmark/database/migrations/2020_03_28_161414_create_books_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            
+            //slug VARCHAR
+            $table->string('slug');
+            
+            //title VARCHAR
+            $table->string('title');
+            
+            //author VARCHAR
+            $table->string('author');
+            
+            //published_year TINYINT
+            $table->tinyInteger('published_year');
+            
+            //cover_url VARCHAR
+            $table->string('cover_url');
+            
+            //info_url VARCHAR
+            $table->string('info_url');
+            
+            //purchase_url VARCHAR
+            $table->string('purchase_url');
+            
+            //description TEXT
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
